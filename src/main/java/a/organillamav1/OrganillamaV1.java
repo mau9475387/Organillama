cb2e097f40c2efa5bf50e21a2102c36f1db7f42f
Mauricio Almendras Ampuero <106159098+mau9475387@users.noreply.github.com>
2022-09-14 10:39:39 -0400

    inicio oficial

--- src/main/java/a/organillamav1/OrganillamaV1.java
+++ src/main/java/a/organillamav1/OrganillamaV1.java
@@ -13,5 +13,6 @@ public class OrganillamaV1 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("bienvenidos a organillama");
     }
 }
