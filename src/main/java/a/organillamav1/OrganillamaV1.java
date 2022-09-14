fdb82af127e547926fc14e8fcc3d8fce12d837f5
Mauricio Almendras Ampuero <106159098+mau9475387@users.noreply.github.com>
2022-09-14 10:32:22 -0400

    inicio

---
+++ src/main/java/a/organillamav1/OrganillamaV1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package a.organillamav1;
+
+/**
+ *
+ * @author Mau
+ */
+public class OrganillamaV1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
